ae4b1461f8bf383b64ff2deb1c037ca03d0bc6b4
ID-0052 <22pwdsc0052@uetpeshawar.edu.pk>
2025-02-25 19:25:19 +0500

    Update

--- backend/server.js
+++ backend/server.js
@@ -86,5 +86,5 @@ app.put("/api/recipes/:id", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
