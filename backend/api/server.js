9fa59c64238b232807e7b7f5976888d8e4d226f3
ID-0052 <22pwdsc0052@uetpeshawar.edu.pk>
2025-02-25 18:59:13 +0500

    U

--- backend/api/server.js
+++ backend/api/server.js
@@ -27,3 +27,4 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/recipes", recipeRoutes);
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+console.log("Server.js executed!");
