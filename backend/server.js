e1ac547e61db9720d7ed64dee94052c637341ad2
ID-0052 <22pwdsc0052@uetpeshawar.edu.pk>
2025-02-25 19:41:34 +0500

    u

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 const Recipe = require("./models/recipe");
 
 const app = express();
